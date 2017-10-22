f7d30039ed94f21e100262e4ec9ed425cf0c432f
Srikrishnan <v.sriku@gmail.com>
2017-10-22 14:46:25 +0530

    Add creative writing project

---
+++ IM/im.js
@@ -0,0 +1,29 @@
+var a = 1;
+$( document ).ready(function() {
+    $("#myModal").modal();
+    $("#myModal").on('hidden.bs.modal', function () {
+        $('#a'+a).popover('toggle');
+		
+		$('.next').click(function(){
+			$('#a'+a).popover('toggle');
+			a=a+1;
+			if(a==21)
+			{
+				a=1;
+			}
+			$('#a'+a).popover('toggle');
+		});
+
+		$('.prev').click(function(){
+			$('#a'+a).popover('toggle');
+			a=a-1;
+			if(a==0)
+			{
+				a=20;
+			}
+			$('#a'+a).popover('toggle');
+		});
+
+
+    });
+});
